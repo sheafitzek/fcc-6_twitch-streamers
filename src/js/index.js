020f48c62070bf0c0de4c8aeb6a40c3cdd8b77c9
Shea Fitzek <agg817@sbcglobal.net>
2017-08-02 23:23:49 -0500

    added ajax

--- src/js/index.js
+++ src/js/index.js
@@ -4,27 +4,74 @@
 
 // ###
 
-let data = null;
+const app = {
+	data: [],
 
-function getChannels() {
-	const cacheBusterNum = 999999;
-	const head = document.querySelector(`head`);
+	getChannels() {
+		const cacheBusterNum = 999999;
+		const head = document.querySelector(`head`);
 
-	const requestBase = `https://wind-bow.gomix.me/twitch-api`;
-	const requestType = `/streams`;
-	const requestUser = [`/freecodecamp`, `/ESL_SC2`, `/OgamingSC2`, `/cretetion`, `/storbeck`, `/habathcx`, `/RobotCaleb`, `/noobs2ninjas`];
-	const requestId = `requestId=${Math.floor(Math.random() * cacheBusterNum).toString()}`; // 'cache-buster'
-	const requestCallback = `&callback=setChannels`;
+		const requestBase = `https://wind-bow.gomix.me/twitch-api`;
+		const requestType = `/streams`;
+		const requestId = `requestId=${Math.floor(Math.random() * cacheBusterNum).toString()}`; // 'cache-buster'
+		const requestCallback = `&callback=app.setChannels`;
 
-	requestUser.forEach((item, index)=> {
-		const apiScript = document.createElement(`script-${index}`);
+		app.streamers.forEach((item, index)=> {
+			const apiScript = document.createElement(`script`);
 
-		apiScript.source = `${requestBase}${requestType}${requestUser}?${requestId}${requestCallback}`;
+			apiScript.src = `${requestBase}${requestType}${item}?${requestId}${requestCallback}`;
 
-		head.appendChild(apiScript);
-	});
-}
+			head.appendChild(apiScript);
+		});
+	},
 
-function setChannels(payload) {
-	data = payload;
-}
+	onloadFunction() {
+		app.getChannels();
+	},
+
+	refresh() {
+		
+	},
+
+	showAll() {
+		
+	},
+
+	showOffline() {
+		
+	},
+
+	showOnline() {
+		
+	},
+
+	setChannels(payload) {
+		app.data.push(payload);
+
+		const streams = document.querySelector(`.streams`);
+		const streamItem = document.createElement(`div`);
+		const streamer = `${app.streamers[app.data.length - 1].slice(1)}`;
+
+		streamItem.className = `streamItem`;
+		streamItem.innerHTML = `
+			<div class="subStream">
+				<h1>${streamer}</h1>
+			</div>
+		`;
+
+		streams.appendChild(streamItem);
+	},
+
+	streamers: [
+		`/cretetion`,
+		`/ESL_SC2`,
+		`/freecodecamp`,
+		`/habathcx`,
+		`/noobs2ninjas`,
+		`/OgamingSC2`,
+		`/RobotCaleb`,
+		`/storbeck`,
+	],
+};
+
+window.onload = app.onloadFunction;
