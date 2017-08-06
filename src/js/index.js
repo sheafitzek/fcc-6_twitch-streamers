c787850662c45f0cb9d91ab40860b3474d160121
Shea Fitzek <agg817@sbcglobal.net>
2017-08-06 11:06:11 -0500

    finished functionality

--- src/js/index.js
+++ src/js/index.js
@@ -40,38 +40,39 @@ const app = {
 	},
 
 	refresh() {
-		
-	},
+		const streams = document.querySelector(`.streams`);
+		const streamItems = document.querySelectorAll(`.streamItem`);
 
-	showAll() {
-		
-	},
+		streamItems.forEach(()=> {
+			streams.removeChild(streams.lastChild);
+		});
 
-	showOffline() {
-		
-	},
-
-	showOnline() {
-		
+		app.getChannels();
 	},
 
 	setChannels(payload) {
 		app.data.push(payload);
+
+		const head = document.querySelector(`head`);
+		const script = head.querySelector(`script`);
 
 		const currentData = app.data[app.data.length - 1];
 		const streams = document.querySelector(`.streams`);
 		const streamItem = document.createElement(`div`);
 		const streamer = `${currentData._links.self
 			.split(`/`).pop()}`;
+		const channel = `https://www.twitch.tv/${streamer}`;
 
 		streamItem.className = `streamItem`;
 
 		if (payload.stream === null) {
 			streamItem.innerHTML = `
-				<div class="subStream">
-					<img src="../media/images/${streamer}.jpeg"/>
-					<h1>${streamer}</h1>
-					<h3>inactive</h3>
+				<div class="subStream inactive">
+					<img src="../media/images/${streamer}.png"/>
+					<a href="${channel}" rel="noopener noreferrer" target="_blank">
+						<h1>${streamer}</h1>
+					</a>
+					<h2>inactive</h2>
 				</div>
 			`;
 		} else {
@@ -79,17 +80,61 @@ const app = {
 			const content = currentData.stream.channel.status;
 
 			streamItem.innerHTML = `
-				<div class="subStream">
-					<h1>${streamer}</h1>
-					<h3>${game}</h3>
-					<h4>${content}</h4>
+				<div class="subStream active">
+					<img src="../media/images/${streamer}.png"/>
+					<a href="${channel}" rel="noopener noreferrer" target="_blank">
+						<h1>${streamer}</h1>
+					</a>
+					<h2>Game:<br>${game}</h2>
+					<h3>Status:<br>${content}</h3>
 				</div>
 			`;
 		}
 
 		streams.appendChild(streamItem);
 
+		head.removeChild(script);
+
 		app.data = [];
+	},
+
+	showAll() {
+		const active = document.querySelectorAll(`.active`);
+		const inactive = document.querySelectorAll(`.inactive`);
+
+		active.forEach((item)=> {
+			item.parentElement.style.display = `block`;
+		});
+
+		inactive.forEach((item)=> {
+			item.parentElement.style.display = `block`;
+		});
+	},
+
+	showOffline() {
+		const active = document.querySelectorAll(`.active`);
+		const inactive = document.querySelectorAll(`.inactive`);
+
+		active.forEach((item)=> {
+			item.parentElement.style.display = `none`;
+		});
+
+		inactive.forEach((item)=> {
+			item.parentElement.style.display = `block`;
+		});
+	},
+
+	showOnline() {
+		const active = document.querySelectorAll(`.active`);
+		const inactive = document.querySelectorAll(`.inactive`);
+
+		active.forEach((item)=> {
+			item.parentElement.style.display = `block`;
+		});
+
+		inactive.forEach((item)=> {
+			item.parentElement.style.display = `none`;
+		});
 	},
 };
 
